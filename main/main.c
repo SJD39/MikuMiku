46a74d39c9db1af25ea2bcef983a1b60a6ff5e5b
SJD39 <sanjiudao@qq.com>
2023-12-26 21:15:19 +0800

    Revert "Revert "buyaod""
    This reverts commit 54e5b4706e3d357943f5d2cde722e797af4cb2bd.

--- main/main.c
+++ main/main.c
@@ -12,7 +12,7 @@ int touch_posi;
 void app_main(void)
 {
     // 配置USB
-    tinyusb_driver_install(&tusb_cfg);
+    // tinyusb_driver_install(&tusb_cfg);
 
     // 初始化触摸
     initializeTouch();
@@ -24,45 +24,45 @@ void app_main(void)
 
     while (1)
     {
-        readTouchData(&touch_value);
+        // readTouchData(&touch_value);
 
         // 判断是否有键被按下
-        for (size_t i = 0; i < TOUCH_BUTTON_NUM; i++)
-        {
-            if (touch_value[i] > 10000)
-            {
-                // 获取触摸位置
-                touch_posi = get_touch_posi(touch_value);
-                vTaskDelay(10 / portTICK_PERIOD_MS);
-                readTouchData(&touch_value);
-                printf("%d\n", abs(touch_posi - get_touch_posi(touch_value)));
-                if (abs(touch_posi - get_touch_posi(touch_value)) < 10)
-                {
-                    printf("%s\n","点按");
-                    if (touch_posi >= 0 && touch_posi < 25)
-                    {
-                        printf("%s\n","x");
-                    }
-                    if (touch_posi >= 25 && touch_posi < 50)
-                    {
-                        printf("%s\n","y");
-                    }
-                    if (touch_posi >= 50 && touch_posi < 75)
-                    {
-                        printf("%s\n","b");
-                    }
-                    if (touch_posi >= 75 && touch_posi < 100)
-                    {
-                        printf("%s\n","a");
-                    }
-                }else{
-                    printf("%s\n","滑键");
-                }
+        // for (size_t i = 0; i < TOUCH_BUTTON_NUM; i++)
+        // {
+        //     if (touch_value[i] > 1000)
+        //     {
+        //         // 获取触摸位置
+        //         touch_posi = get_touch_posi(touch_value);
+        //         vTaskDelay(10 / portTICK_PERIOD_MS);
+        //         readTouchData(&touch_value);
+        //         printf("%d\n", abs(touch_posi - get_touch_posi(touch_value)));
+        //         if (abs(touch_posi - get_touch_posi(touch_value)) < 10)
+        //         {
+        //             printf("%s\n","点按");
+        //             if (touch_posi >= 0 && touch_posi < 25)
+        //             {
+        //                 printf("%s\n","x");
+        //             }
+        //             if (touch_posi >= 25 && touch_posi < 50)
+        //             {
+        //                 printf("%s\n","y");
+        //             }
+        //             if (touch_posi >= 50 && touch_posi < 75)
+        //             {
+        //                 printf("%s\n","b");
+        //             }
+        //             if (touch_posi >= 75 && touch_posi < 100)
+        //             {
+        //                 printf("%s\n","a");
+        //             }
+        //         }else{
+        //             printf("%s\n","滑键");
+        //         }
 
-                break;
-            }
-        }
+        //         break;
+        //     }
+        // }
 
-        vTaskDelay(10 / portTICK_PERIOD_MS);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
